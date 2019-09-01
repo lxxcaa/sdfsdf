b5e20850fd7be8559ad728fdee19d75d57fbeaef
Glitch (hello-express) <none>
2019-09-01 20:21:13 +0000

    💪🍏 Checkpoint ./server.js:6459426/5888

--- server.js
+++ server.js
@@ -5,41 +5,49 @@
 const express = require('express');
 const app = express();
 
-const { Client, RichEmbed } = require('discord.js');
+const {
+    Client,
+    RichEmbed
+} = require('discord.js');
 var client = new Client();
-var token = "NjE3NzgyNTg1NTk0MjE2NDQ4.XWwI9w.RhCEqTD30HpJCmij0P9McxQZq5E"+"/exec"
-var scriptID = "AKfycbxQeVF4720epXP3Bc-x0vP1V6MUBKcMj3dy358XTdhewKiluHDY"
-async function startApp () {
+var token = "NjE3NzgyNTg1NTk0MjE2NDQ4.XWwI9w.RhCEqTD30HpJCmij0P9McxQZq5E"
+var scriptID = "AKfycbxQeVF4720epXP3Bc-x0vP1V6MUBKcMj3dy358XTdhewKiluHDY" + "/exec"
+async function startApp() {
     client.login(token)
     console.log("Successfully logged Discord bot in");
 }
 startApp();
 client.on("ready", () => {
-  console.log("Ready");
+    console.log("Ready");
 })
-          
+
 var prefix = ';';
 
-function isCommand(command, message ){
-	var command = command.toLowerCase();
-	var content = message.content.toLowerCase();
-	return content.startsWith(prefix + command);
+function isCommand(command, message) {
+    var command = command.toLowerCase();
+    var content = message.content.toLowerCase();
+    return content.startsWith(prefix + command);
 }
 client.on('message', (message) => {
-	if (message.author.id != 617782585594216448) { // Dont answer yourself.
-      const args = message.content.slice(prefix.length).split(' ');
-      if (isCommand("Ban",message)) {
-        console.log("Banning player " + args[1]);
-        message.channel.send("Banning player " + args[1]);
-  $.ajax({
-  type: "POST",
-  url: "https://script.google.com/macros/s/AKfycbxQeVF4720epXP3Bc-x0vP1V6MUBKcMj3dy358XTdhewKiluHDY/exec",
-  headers: {
-    "Content-Type":"application/x-www-form-urlencoded",
-  }
-    else if (isCommand("Test",message)) {
-        console.log("Test command");
-      }
+    if (message.author.id != 617782585594216448) { // Dont answer yourself.
+        const args = message.content.slice(prefix.length).split(' ');
+        if (isCommand("Ban", message)) {
+            console.log("Banning player " + args[1]);
+            message.channel.send("Banning player " + args[1]);
+            var userid = args[1]
+            $.ajax({
+                url: "https://script.google.com/macros/s/" + scriptID,
+                data: {
+                    "sheet=": "Global",
+                    "&key": userid,
+                    "&value": true
+                },
+                type: "POST",
+                dataType: "xml",
+            });
+        } else if (isCommand("Test", message)) {
+            console.log("Test command");
+        }
     }
 });
 
@@ -51,14 +59,14 @@ app.use(express.static('public'));
 //
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+    response.sendFile(__dirname + '/views/index.html');
 });
 
 // listen for requests :)
 const http = require('http');
 var listener = app.listen(process.env.PORT, function() {
-  setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-  }, 280000);
-  console.log('Your app is listening on port ' + listener.address().port);
+    setInterval(() => {
+        http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    }, 280000);
+    console.log('Your app is listening on port ' + listener.address().port);
 });

45b3fc1405efd8fdc8a97d4a27374c2b2d3314a1
Glitch (hello-express) <none>
2020-05-01 04:35:37 +0000

    🎰⚡️ Checkpoint ./server.js:5483495/48

--- server.js
+++ server.js
@@ -112,8 +112,8 @@ app.use(express.static('public'));
 
 app.get('/', function(request, response) {
   if (request.headers.username != undefined) { 
-    const channel = client.channels.cache.get(request.headers.cid);
-    if (request.headers.rblxerror != undefined) {
+    const channel = client.channels.get(request.headers.cid);
+    if (request.headers.rblxerror == undefined) {
       channel.send('Successfully ' + request.headers.method + ' user ' + request.headers.username + " | ID: " + request.headers.value);
     } else {
       channel.send("Failed to " + request.headers.method + " user: " + request.headers.username + " | ID: " + request.headers.value + " | `Rblx-Error:  " + request.headers.rblxerror + "`"); 
